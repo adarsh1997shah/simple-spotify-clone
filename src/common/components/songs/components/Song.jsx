6e6a37b4cd5b8a83c48ebc1b0a69b820225fdd4f
adarsh1997shah <adarshshah01@gmail.com>
2023-09-08 22:44:17 +0530

    feat: added playing music gif when music played

--- src/common/components/songs/components/Song.jsx
+++ src/common/components/songs/components/Song.jsx
@@ -1,68 +1,77 @@
 import PropTypes from 'prop-types';
 import { useSelector } from 'react-redux';
 import {
-  Avatar,
-  ListItem,
-  ListItemAvatar,
-  ListItemButton,
-  ListItemText,
+	Avatar,
+	ListItem,
+	ListItemAvatar,
+	ListItemButton,
+	ListItemText,
+	Typography,
 } from '@mui/material';
 
 import { getMinAndSec } from '@/common/utils/time';
 
+import playingMusic from '@/assets/gifs/playing-music.gif';
+
 function Song({ song, onClick }) {
-  const { currentlyPlaying } = useSelector(state => state.music);
+	const { currentlyPlaying } = useSelector((state) => state.music);
 
-  const { _id, title, photo, duration, artist } = song;
+	const { _id, title, photo, duration, artist } = song;
 
-  const { min, seconds } = getMinAndSec(duration);
+	const { min, seconds } = getMinAndSec(duration);
 
-  return (
-    <ListItemButton
-      key={_id}
-      selected={currentlyPlaying?._id == _id}
-      sx={{
-        pr: 0,
-        borderRadius: 2,
-        transition: 'all 200ms linear',
-        ':hover': {
-          backgroundColor: 'rgba(25, 118, 210, 0.08)',
-        },
-      }}
-      onClick={onClick}
-    >
-      <ListItem
-        disablePadding
-        secondaryAction={
-          <div>
-            {min}:{seconds}
-          </div>
-        }
-      >
-        <ListItemAvatar>
-          <Avatar src={photo} alt={title} />
-        </ListItemAvatar>
-        <ListItemText
-          primary={title}
-          secondary={artist}
-          sx={{
-            '& p': {
-              color: '#fff',
-              opacity: '0.6',
-            },
-            '& span': {
-              width: '85%',
-            },
-          }}
-        />
-      </ListItem>
-    </ListItemButton>
-  );
+	const isCurrentlyPlaying = currentlyPlaying?._id == _id;
+
+	return (
+		<ListItemButton
+			key={_id}
+			selected={isCurrentlyPlaying}
+			sx={{
+				borderRadius: 2,
+				transition: 'all 200ms linear',
+				':hover': {
+					backgroundColor: 'rgba(25, 118, 210, 0.08)',
+				},
+			}}
+			onClick={onClick}>
+			<ListItem disablePadding>
+				<ListItemAvatar>
+					<Avatar src={photo} alt={title} />
+				</ListItemAvatar>
+				<ListItemText
+					primary={title}
+					secondary={artist}
+					sx={{
+						'& p': {
+							color: '#fff',
+							opacity: '0.6',
+						},
+						'& span': {
+							width: '85%',
+						},
+					}}
+				/>
+
+				{isCurrentlyPlaying && (
+					<Avatar
+						sx={{ width: 30, height: 30, mr: 3 }}
+						src={playingMusic}
+						alt="audio-playing"
+						variant="square"
+					/>
+				)}
+
+				<Typography variant="subtitle1">
+					{min}:{seconds}
+				</Typography>
+			</ListItem>
+		</ListItemButton>
+	);
 }
 
 Song.propTypes = {
-  song: PropTypes.object,
-  onClick: PropTypes.func,
+	song: PropTypes.object,
+	onClick: PropTypes.func,
 };
 
 export default Song;
