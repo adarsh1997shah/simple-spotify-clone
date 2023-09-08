d838fc46464640d7cd0667e8bf54279935226497
adarsh1997shah <adarshshah01@gmail.com>
2023-09-08 21:10:14 +0530

    feat: added loader when audio loading

--- src/common/components/audioPlayer/index.jsx
+++ src/common/components/audioPlayer/index.jsx
@@ -1,8 +1,8 @@
-import { useEffect, useRef } from 'react';
+import { useEffect, useRef, useState } from 'react';
 import AudioPlayer, { RHAP_UI } from 'react-h5-audio-player';
 import { useDispatch, useSelector } from 'react-redux';
 import { Box } from '@mui/system';
-import { Avatar, Typography } from '@mui/material';
+import { Avatar, CircularProgress, Typography } from '@mui/material';
 
 import { Next } from '@/assets/svg/next';
 import { Previous } from '@/assets/svg/previous';
@@ -14,117 +14,125 @@ import 'react-h5-audio-player/lib/styles.css';
 import './index.css';
 
 function CustomAudioPlayer() {
-  const playerRef = useRef();
+	const [isAudioLoding, setIsAudioLoading] = useState(false);
 
-  const dispatch = useDispatch();
-  const { currentlyPlaying, currentPlaylist } = useSelector(
-    state => state.music
-  );
+	const playerRef = useRef();
 
-  useEffect(() => {
-    const audioRef = playerRef.current.audio;
+	const dispatch = useDispatch();
+	const { currentlyPlaying, currentPlaylist, data } = useSelector((state) => state.music);
 
-    if (currentlyPlaying) {
-      audioRef.current?.load();
-      audioRef.current?.play();
-    }
+	useEffect(() => {
+		const audioRef = playerRef.current.audio;
 
-    return () => {
-      if (currentlyPlaying) {
-        audioRef.current?.pause();
-      }
-    };
-  }, [currentlyPlaying]);
+		if (currentlyPlaying) {
+			audioRef.current?.load();
+			audioRef.current?.play();
+		}
 
-  const handlePrevious = () => {
-    const index = currentPlaylist.findIndex(
-      ({ _id }) => _id == currentlyPlaying._id
-    );
+		setIsAudioLoading(true);
 
-    if (index > -1 && index != 0) {
-      dispatch(setCurrentlyPlaying(currentPlaylist[index - 1]));
-    }
-  };
+		return () => {
+			if (currentlyPlaying) {
+				audioRef.current?.pause();
+			}
+		};
+	}, [currentlyPlaying]);
 
-  const handleNext = () => {
-    const index = currentPlaylist.findIndex(
-      ({ _id }) => _id == currentlyPlaying._id
-    );
+	useEffect(() => {
+		if (data.length > 0) {
+			setIsAudioLoading(false);
+		}
+	}, [data]);
 
-    if (index > -1 && index != currentPlaylist.length - 1) {
-      dispatch(setCurrentlyPlaying(currentPlaylist[index + 1]));
-    }
-  };
+	const handlePrevious = () => {
+		const index = currentPlaylist.findIndex(({ _id }) => _id == currentlyPlaying._id);
 
-  const handleError = () => {
-    dispatch(openSnackbar({ severity: 'error', msg: 'Media error occurred' }));
-  };
+		if (index > -1 && index != 0) {
+			dispatch(setCurrentlyPlaying(currentPlaylist[index - 1]));
+		}
+	};
 
-  return (
-    <Box
-      px={{ mg: 0, lg: 12, height: '100%' }}
-      display="flex"
-      flexDirection="column"
-      justifyContent="center"
-      className="audio-player-container"
-    >
-      <Box display="flex" gap={4}>
-        <Avatar
-          alt="Remy Sharp"
-          src={currentlyPlaying?.photo}
-          sx={{ display: { md: 'none' }, width: 100, height: 100 }}
-          variant="rounded"
-        />
-        <Box>
-          <Typography variant="h4" sx={{ fontWeight: 700 }}>
-            {currentlyPlaying?.title}
-          </Typography>
+	const handleNext = () => {
+		const index = currentPlaylist.findIndex(({ _id }) => _id == currentlyPlaying._id);
 
-          <Typography variant="caption" sx={{ opacity: 0.4, fontSize: 16 }}>
-            {currentlyPlaying?.artist}
-          </Typography>
-        </Box>
-      </Box>
+		if (index > -1 && index != currentPlaylist.length - 1) {
+			dispatch(setCurrentlyPlaying(currentPlaylist[index + 1]));
+		}
+	};
 
-      <Box
-        className="cover-image-container"
-        sx={{
-          bgcolor: 'rgba(255,255,255,0.1)',
-          backdropFilter: 'blur(40px)',
-          height: { sm: '100%', md: '300px' },
-          width: '100%',
-          borderRadius: 2,
-          display: { xs: 'none', md: 'block' },
-        }}
-        mt={2}
-      >
-        {currentlyPlaying?.photo ? (
-          <img src={currentlyPlaying.photo} alt="cover-image" />
-        ) : (
-          ''
-        )}
-      </Box>
+	const handleError = () => {
+		dispatch(openSnackbar({ severity: 'error', msg: 'Media error occurred' }));
+	};
 
-      <AudioPlayer
-        className={`${currentlyPlaying ? '' : 'audio-disable'}`}
-        ref={playerRef}
-        src={currentlyPlaying?.url}
-        onClickPrevious={handlePrevious}
-        onClickNext={handleNext}
-        showSkipControls
-        showJumpControls={false}
-        customIcons={{ previous: <Previous />, next: <Next /> }}
-        defaultCurrentTime="00:00"
-        defaultDuration="00:00"
-        customProgressBarSection={[
-          RHAP_UI.PROGRESS_BAR,
-          RHAP_UI.CURRENT_TIME,
-          RHAP_UI.DURATION,
-        ]}
-        onError={handleError}
-      />
-    </Box>
-  );
+	return (
+		<Box
+			px={{ mg: 0, lg: 12, height: '100%' }}
+			display="flex"
+			flexDirection="column"
+			justifyContent="center"
+			className="audio-player-container">
+			<Box display="flex" gap={4}>
+				<Avatar
+					alt="Remy Sharp"
+					src={currentlyPlaying?.photo}
+					sx={{ display: { md: 'none' }, width: 100, height: 100 }}
+					variant="rounded"
+				/>
+				<Box>
+					<Typography variant="h4" sx={{ fontWeight: 700 }}>
+						{currentlyPlaying?.title}
+					</Typography>
+
+					<Typography variant="caption" sx={{ opacity: 0.4, fontSize: 16 }}>
+						{currentlyPlaying?.artist}
+					</Typography>
+				</Box>
+			</Box>
+
+			<Box
+				className="cover-image-container"
+				sx={{
+					bgcolor: 'rgba(255,255,255,0.1)',
+					backdropFilter: 'blur(40px)',
+					height: { sm: '100%', md: '300px' },
+					width: '100%',
+					borderRadius: 2,
+					display: { xs: 'none', md: 'block' },
+				}}
+				mt={2}>
+				{currentlyPlaying?.photo ? (
+					<img src={currentlyPlaying.photo} alt="cover-image" />
+				) : (
+					''
+				)}
+			</Box>
+
+			<AudioPlayer
+				className={`${currentlyPlaying ? '' : 'audio-disable'}`}
+				ref={playerRef}
+				src={currentlyPlaying?.url}
+				onClickPrevious={handlePrevious}
+				onClickNext={handleNext}
+				showSkipControls
+				showJumpControls={false}
+				customIcons={{ previous: <Previous />, next: <Next /> }}
+				defaultCurrentTime="00:00"
+				defaultDuration="00:00"
+				customProgressBarSection={[
+					RHAP_UI.PROGRESS_BAR,
+					RHAP_UI.CURRENT_TIME,
+					RHAP_UI.DURATION,
+				]}
+				customControlsSection={[
+					RHAP_UI.ADDITIONAL_CONTROLS,
+					isAudioLoding ? <CircularProgress color="inherit" /> : RHAP_UI.MAIN_CONTROLS,
+					RHAP_UI.VOLUME_CONTROLS,
+				]}
+				onError={handleError}
+				onCanPlay={() => setIsAudioLoading(false)}
+			/>
+		</Box>
+	);
 }
 
 export default CustomAudioPlayer;
