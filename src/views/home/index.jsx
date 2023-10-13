f711a2e4ff34ea8dc7f65fe0dbcdcc948c7c29df
adarsh1997shah <adarshshah01@gmail.com>
2023-10-14 02:44:23 +0530

    fix: use tiny color to manipulate background color

--- src/views/home/index.jsx
+++ src/views/home/index.jsx
@@ -3,14 +3,13 @@ import { Box, Grid } from '@mui/material';
 import { Outlet } from 'react-router-dom';
 import { useDispatch, useSelector } from 'react-redux';
 import { motion } from 'framer-motion';
-import ColorThief from 'colorthief';
 
 import Navbar from '@/common/components/Navbar';
 import Search from '@/common/components/Search';
 import AudioPlayer from '@/common/components/audioPlayer';
 
 import { openSnackbar } from '@/reducers/snackbar';
-import { getBackgroundColor } from './utils';
+import { getBackgroundColor, getColor } from './utils';
 import Tab from './components/Tab';
 
 function Home() {
@@ -22,13 +21,13 @@ function Home() {
 	const getColorFromImage = useCallback(
 		(currentlyPlaying) => {
 			try {
-				const colorThief = new ColorThief();
 				const img = new Image();
 				img.crossOrigin = 'Anonymous'; // Setting it to prevent image url to be painted on canvas without CORS issue, but some image still have it.
 
 				img.addEventListener('load', () => {
-					const rgb = colorThief.getColor(img);
-					setColors((prev) => ({ initial: prev.current, current: rgb }));
+					const { r, g, b } = getColor(img);
+
+					setColors((prev) => ({ initial: prev.current, current: [r, g, b] }));
 				});
 
 				img.src = currentlyPlaying.photo;
@@ -58,10 +57,8 @@ function Home() {
 			}}
 			initial={false}
 			transition={{ delay: 0.1, duration: 0.3, ease: 'linear' }}
-			sx={{
-				minHeight: '100vh',
-				color: 'white',
-			}}>
+			sx={{ minHeight: '100vh', color: 'white' }}
+		>
 			<Grid container spacing={2}>
 				<Grid item xs={0} sm={4} md={2.5} sx={{ display: { xs: 'none', sm: 'block' } }}>
 					<Navbar />
@@ -74,7 +71,8 @@ function Home() {
 					xs={12}
 					sm={8}
 					md={9.5}
-					direction={{ md: 'row-reverse' }}>
+					direction={{ md: 'row-reverse' }}
+				>
 					<Grid item xs={12} md={6} lg={7}>
 						<AudioPlayer />
 					</Grid>
@@ -85,7 +83,8 @@ function Home() {
 								className="music-list-nav-wrap"
 								display="flex"
 								gap={5}
-								px={{ xs: 0, md: 2 }}>
+								px={{ xs: 0, md: 2 }}
+							>
 								<Tab to="/for-you" title="For You" />
 
 								<Tab to="/top-tracks" title="Top Tracks" />
